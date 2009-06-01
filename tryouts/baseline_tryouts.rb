734b65ee750c7964bb70996866eff7fc84b94ef1
delano <delano@solutious.com>
2009-06-01 07:02:46 -0400

    Added drill for forced_array block

--- tryouts/baseline_tryouts.rb
+++ tryouts/baseline_tryouts.rb
@@ -1,39 +1,44 @@
 LIBRARY_PATH = File.expand_path(File.join(File.dirname(__FILE__), "..", "lib", "caesars"))
 
-#group "baseline"
+group "baseline"
 
 library :caesars, LIBRARY_PATH
 
 dreams File.join(GYMNASIUM_HOME, "baseline_dreams.rb")
 tryout "Common Usage", :api do
   setup do
-    @@master = :fluff
+    # Define a Caesars class for all drills to use. Each drill needs to extend
+    # this class since each drill runs in its own DrillContext instance. 
+    class Master < Caesars
+      chill :calc
+      forced_array :fluff
+    end
   end
   
   drill "Create a simple DSL" do
-    class Simple < Caesars; end
-    extend Simple::DSL
-    simple do
+    extend Master::DSL
+    master do
       works :true
     end
-    @simple.works
+    @master.works
   end
   
   drill "Store Block" do
-    class Multiplier < Caesars; chill :calc; end
-    extend Multiplier::DSL
-    multiplier do
+    extend Master::DSL
+    master do
       calc do; end
     end
-    @multiplier.calc
+    @master.calc
   end
   
-  dream "Create a complex DSL", true
-  drill "Create a complex DSL" do
-    p [:dsl, self]
-    p [:master, @@master]
-    true
+  drill "Forced Array blocks are always chilled" do
+    extend Master::DSL
+    master do 
+      fluff :ignore
+      fluff :inline do; end
+    end
+    # We created two fluff elements above. We want the last one which
+    # itself has two elements: [:inline, Proc]. We want the Proc. 
+    @master.fluff.last.last
   end
-  
-  
 end
